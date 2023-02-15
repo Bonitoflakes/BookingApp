6ab5573a9463fd6f0d5d30544ca5cf97c066c2a4
Rishab Khivsara <rishabjain3500@gmail.com>
2023-02-15 20:32:34 +0530

    Fix: Temporary navbar changes to main, TODO: figure out state

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Doubts
 
 Stack is not working as expected. Flight ticket component --> The flight has to be aligned in the center horizontally.
+
+Have to figure out how to share state between parent and child.
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,76 @@
 import 'package:booking_app/bottom_bar.dart';
 import 'package:booking_app/home_page.dart';
 import 'package:booking_app/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final _widgetOptions = [
+    const HomePage(),
+    const Text('Search'),
+    const Text('Tickets'),
+    const Text('Profile'),
+  ];
+
+  int _currentIndex = 0;
+
+  void setCurrentIndex(int value) {
+    setState(() {
+      _currentIndex = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Styles.bgColor,
-          elevation: 0,
-        ),
-        body: HomePage(),
-        bottomNavigationBar: const BottomBar(),
+          appBar: AppBar(
+            backgroundColor: Styles.bgColor,
+            elevation: 0,
+          ),
+          body: _widgetOptions[_currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+                  label: 'Search'),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                  label: 'Tickets'),
+              BottomNavigationBarItem(
+                  icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+                  activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+                  label: 'Profile'),
+            ],
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            enableFeedback: true,
+            selectedItemColor: Styles.primaryColor,
+            unselectedItemColor: Colors.blueGrey,
+            elevation: 10,
+            currentIndex: _currentIndex,
+            onTap: setCurrentIndex,
+            type: BottomNavigationBarType.fixed,
+          )
       ),
     );
   }
